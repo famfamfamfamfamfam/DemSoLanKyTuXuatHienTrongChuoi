8adb24bdb9d8dd4d546c52c201885a1cf378d28a
BinhTran <fam.out16@gmail.com>
2024-08-23 21:30:34 +0700

    Add cac files bai tap viet phuong thuc Dem so lan ky tu xuat hien trong chuoi

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace SUDUNGPHUONGTHUC
+{
+    class DemSoLanKyTuXuatHienTrongChuoi
+    {
+        public static int Solankytuxuathientrongchuoi(string chuoi, char kytu)
+        {
+            int dem = 0;
+            for (int chiso = 0; chiso < chuoi.Length; chiso++)
+            {
+                if (chuoi[chiso] == kytu)
+                {
+                    dem++;
+                }
+            }
+            return dem;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap chuoi:");
+            string chuoi = Console.ReadLine();
+            Console.WriteLine("Nhap ky tu muon dem so lan no xuat hien:");
+            char kytu = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine($"{kytu} xuat hien {Solankytuxuathientrongchuoi(chuoi, kytu)} lan");
+        }
+    }
+}
